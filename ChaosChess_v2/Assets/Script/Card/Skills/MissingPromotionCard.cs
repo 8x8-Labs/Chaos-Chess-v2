401b58187bb5c5282bddb27f7eb4319a049ae023
Lee Siwoo <s24028@gsm.hs.kr>
2026-04-06 13:36:10 +0900

    feat: 진급누락 스킬 카드 추가

---
+++ ChaosChess_v2/Assets/Script/Card/Skills/MissingPromotionCard.cs
@@ -0,0 +1,23 @@
+﻿public class MissingPromotionCard : CardData, IPieceCard
+{
+    private PieceSelector selector;
+
+    private void Awake()
+    {
+        selector = FindFirstObjectByType<PieceSelector>();
+    }
+
+    public void LoadPieceSelector()
+    {
+        if (selector == null) selector = FindFirstObjectByType<PieceSelector>();
+        selector.EnableSelector(this);
+    }
+
+    public void Execute(CardEffectArgs args = null)
+    {
+        Piece p = args.Targets[0];
+
+        BoardManager.Instance.ChangePiece(p.Pos, PieceColor.Black, 'p');
+    }
+
+}
