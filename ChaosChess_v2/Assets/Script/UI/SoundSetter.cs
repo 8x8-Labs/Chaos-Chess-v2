a90853bec54c9ebc8d87ebcc8ecb0dd2c1f5fb76
Lee Siwoo <s24028@gsm.hs.kr>
2026-04-06 20:47:50 +0900

    feat: 사운드 관리자 추가 및 사운드 조정 기능 추가
    #40

---
+++ ChaosChess_v2/Assets/Script/UI/SoundSetter.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundSetter : MonoBehaviour
+{
+    private SoundManager soundManager;
+
+    [SerializeField] private Slider sfxSlider;
+    [SerializeField] private Slider bgmSlider;
+
+    private void Start()
+    {
+        soundManager = SoundManager.Instance;
+    }
+
+    public void EnableSetter()
+    {
+        bgmSlider.value = soundManager.GetBGMVolume();
+        sfxSlider.value = soundManager.GetSFXVolume();
+    }
+
+    public void DisableSetter() { }
+
+    public void SetBGM(float val) => soundManager.BGSoundVolume(val);
+    public void SetSFX(float val) => soundManager.SFXSoundVolume(val);
+}
