19007e34e63dbc20dea180b3f98d0cbb24d0f3c8
sxonz <taebal2020@gmail.com>
2026-04-07 02:47:00 +0900

    feat: 정신 집중 카드 구현 완료

--- ChaosChess_v2/Assets/Script/Card/skills/ConcentrationCard.cs
+++ ChaosChess_v2/Assets/Script/Card/skills/ConcentrationCard.cs
@@ -5,6 +5,24 @@ using UnityEngine;
 /// 정신 집중 - 기물 전용 (전설, 아마존)
 /// 선택 기물은 일정 수치 동안 상호작용할 수 없고, 이후 아마존으로 승격됩니다.
 /// </summary>
+class ConcentrationEffector : PieceEffector
+{
+    Piece piece;
+    public void Init(Piece piece)
+    {
+        this.piece = piece;
+    }
+    public override void Apply()
+    {
+        BoardManager.Instance.ChangePiece(piece.Pos,piece.Color,'a');
+    }
+
+    public override void Revert()
+    {
+        BoardManager.Instance.ChangePiece(piece.Pos,piece.Color,'s');
+        Destroy(this);
+    }
+}
 public class ConcentrationCard : CardData, IPieceCard
 {
     private PieceSelector selector;
@@ -22,8 +40,11 @@ public class ConcentrationCard : CardData, IPieceCard
 
     public void Execute(CardEffectArgs args = null)
     {
-        List<Piece> pieces = args.Targets;
-        // TODO: 선택된 기물을 DataSO.PieceLimitTurn 동안 상호작용 불가 상태로 설정
-        //       이후 아마존(커스텀 기물)으로 승격 처리
+        Piece piece = args.Targets[0];
+
+        var effector = CreatePieceEffector<ConcentrationEffector>(piece);
+        effector.Init(piece);
+        effector.Apply();
+        GameManager.Instance.AppendAction(DataSO.PieceLimitTurn, effector.Revert);
     }
 }
