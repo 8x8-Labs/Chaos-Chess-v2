0975c407b32c5f221a3b0ac5ce3f8e0801e012ff
Lee Siwoo <s24028@gsm.hs.kr>
2026-04-07 15:53:55 +0900

    fix: 풍차 카드 올바르게 변경

--- ChaosChess_v2/Assets/Script/Card/skills/WindmillCard.cs
+++ ChaosChess_v2/Assets/Script/Card/skills/WindmillCard.cs
@@ -1,4 +1,4 @@
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
@@ -33,13 +33,13 @@ public class WindmillCard : CardData, IPieceCard
 public class WindmillEffector : PieceEffector
 {
     public string change;
-    public override void Apply()
+    protected override void OnApply()
     {
         target.FenOverride = change;
         BoardManager.Instance.RefreshMoves();
     }
 
-    public override void Revert()
+    protected override void OnRevert()
     {
         target.FenOverride = null;
         BoardManager.Instance.RefreshMoves();
