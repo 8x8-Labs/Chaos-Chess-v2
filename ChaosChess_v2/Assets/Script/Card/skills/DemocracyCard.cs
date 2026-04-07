550fdb963e59b8f13d65bba29aab49b1eb920504
nelu572 <s25031@gsm.hs.kr>
2026-04-08 01:19:36 +0900

    feat: 민주주의 스킬 카드 구현

---
+++ ChaosChess_v2/Assets/Script/Card/skills/DemocracyCard.cs
@@ -0,0 +1,66 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 민주주의 - 전역 (레어)
+/// 상대 폰의 개수가 상대의 폰을 제외한 기물의 개수가 두배 이상일때 혁명이 일어나 킹을 끌어내립니다. 
+/// 몰락한 왕은 항복을 선언합니다.
+/// </summary>
+public class DemocracyCard : CardData, ICard
+{
+    public void Execute(CardEffectArgs args = null)
+    {
+        Debug.Log("카드 사용");
+        DemocracyEffect effect =
+            CreateGlobalEffector<DemocracyEffect>();
+
+        effect.Apply();
+        effect.CheckCondition();
+    }
+}
+
+public class DemocracyEffect : GlobalEffector
+{
+    protected override void OnApply()
+    {
+        GameManager.Instance.OnTurnChanged += CheckCondition;
+    }
+
+    protected override void OnRevert()
+    {
+        GameManager.Instance.OnTurnChanged -= CheckCondition;
+        Destroy(gameObject);
+    }
+
+    public void OnDestroy()
+    {
+        Revert();
+    }
+
+    public void CheckCondition()
+    {
+        List<Piece> allPieces = BoardManager.Instance.GetAllPieces();
+        int pawnCount = 0;
+        int otherCount = 0;
+
+        foreach (Piece piece in allPieces)
+        {
+            if (piece.Color == GameManager.Instance.EnemyColor)
+            {
+                if (piece.Type == PieceType.Pawn)
+                {
+                    pawnCount++;
+                }
+                else
+                {
+                    otherCount++;
+                }
+            }
+        }
+        Debug.Log($"폰 개수: {pawnCount}, 기타 기물 개수: {otherCount}");
+        if (pawnCount >= 2 * otherCount)
+        {
+            GameManager.Instance.OnSurrender(GameManager.Instance.EnemyColor);
+        }
+    }
+}
