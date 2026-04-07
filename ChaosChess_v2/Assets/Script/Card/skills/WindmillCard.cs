6e84c9a56e49b7f4ecd11bd48e4d669c366f7000
sxonz <taebal2020@gmail.com>
2026-04-07 13:29:32 +0900

    fix: gemini 리뷰 반영

--- ChaosChess_v2/Assets/Script/Card/skills/WindmillCard.cs
+++ ChaosChess_v2/Assets/Script/Card/skills/WindmillCard.cs
@@ -27,8 +27,7 @@ public class WindmillCard : CardData, IPieceCard
         effector.change = piece.Type == PieceType.Rook ? "b" : "r";
         effector.Apply();
         GameManager.Instance.AppendAction(DataSO.PieceLimitTurn, effector.Revert);
-        // pieces[0]: 룩 또는 비숍
-        // TODO: 선택된 룩/비숍의 행마법을 DataSO.PieceLimitTurn(2)턴 동안 서로 교환 처리
+
     }
 }
 public class WindmillEffector : PieceEffector
@@ -36,7 +35,6 @@ public class WindmillEffector : PieceEffector
     public string change;
     public override void Apply()
     {
-        Debug.Log(change);
         target.FenOverride = change;
         BoardManager.Instance.RefreshMoves();
     }
@@ -44,7 +42,7 @@ public class WindmillEffector : PieceEffector
     public override void Revert()
     {
         target.FenOverride = null;
-        BoardManager.Instance. RefreshMoves();
+        BoardManager.Instance.RefreshMoves();
         Destroy(this);
     }
 
