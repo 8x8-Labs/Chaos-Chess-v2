3259a870d3db371d6e02652b94b2c1c1bc8031d5
Lee Siwoo <s24028@gsm.hs.kr>
2026-04-08 10:17:48 +0900

    feat: 하극상 스킬 카드 추가

--- ChaosChess_v2/Assets/Script/Card/Skills/MutinyCard.cs
+++ ChaosChess_v2/Assets/Script/Card/Skills/MutinyCard.cs
@@ -1,4 +1,5 @@
-﻿
+﻿using System.Linq;
+
 /// <summary>
 /// 하극상 - 기물 전용 (레어)
 /// 상대의 퀸은 3턴 동안 전반향으로 한칸밖에 이동할 수 없습니다.
@@ -7,6 +8,26 @@ public class MutinyCard : CardData, ICard
 {
     public void Execute(CardEffectArgs args = null)
     {
-        throw new System.NotImplementedException();
+        Piece queen = BoardManager.Instance.GetPiece<Queen>(
+            GameManager.Instance.EnemyColor).FirstOrDefault();
+
+        MutinyEffect effect = CreatePieceEffector<MutinyEffect>(queen);
+        effect.Apply();
+    }
+}
+
+public class MutinyEffect : PieceEffector
+{
+    protected override void OnApply()
+    {
+        target.MoveFenOverride = "w";
+        BoardManager.Instance.RefreshMoves();
+    }
+
+    protected override void OnRevert()
+    {
+        target.MoveFenOverride = null;
+        BoardManager.Instance.RefreshMoves();
+        Destroy(this);
     }
 }
