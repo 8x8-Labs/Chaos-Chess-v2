0c42192a6c6edbdb4e99a87aa52750872405d460
sxonz <203660235+sxonz@users.noreply.github.com>
2026-04-10 23:33:26 +0900

    feat: 윤회 카드 구현 완료

--- ChaosChess_v2/Assets/Script/Card/Skills/TransmigrationCard.cs
+++ ChaosChess_v2/Assets/Script/Card/Skills/TransmigrationCard.cs
@@ -23,10 +23,33 @@ public class TransmigrationCard : CardData, IPieceCard
     public void Execute(CardEffectArgs args = null)
     {
         Piece piece = args.Targets[0];
-        Debug.Log(piece.StartPos);
         if (piece.IsPromotioned)
         {
-            Debug.Log("promotion");
+            Vector3Int sp = piece.StartPos;
+            Piece cur = BoardManager.Instance.GetPiece(sp);
+            if (cur != null)
+            {
+                int[] dx = { -1, -1, -1, 0, 1, 1, 1, 0 };
+                int[] dy = { -1, 0, 1, 1, 1, 0, -1, -1 };
+                bool flag = true;
+                for(int i = 0; i < 8; i++)
+                {
+                    int x = dx[i];
+                    int y = dy[i];
+                    Vector3Int can = new Vector3Int(sp.x + x, sp.y + y, sp.z);
+                    Piece p = BoardManager.Instance.GetPiece(can);
+                    if (p == null && flag)
+                    {
+                        Debug.Log("moved");
+                        BoardManager.Instance.ForceTeleport(cur,can);
+                        BoardManager.Instance.RefreshMoves();
+                        flag = false;
+                    }
+                }
+            }
+            BoardManager.Instance.ForceTeleport(piece, sp);
+            BoardManager.Instance.ChangePiece(sp, GameManager.Instance.turnColor == PieceColor.White ? PieceColor.Black : PieceColor.White, 'p',piece);
+            BoardManager.Instance.RefreshMoves();
         }
 
     }
