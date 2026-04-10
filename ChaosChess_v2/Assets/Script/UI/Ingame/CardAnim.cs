e37d4d712672e4208d23a0094db1a15f27ad8ac6
Lee Siwoo <s24028@gsm.hs.kr>
2026-04-10 23:54:05 +0900

    feat: 인게임 카드 애니메이션 추가
    #15

---
+++ ChaosChess_v2/Assets/Script/UI/Ingame/CardAnim.cs
@@ -0,0 +1,23 @@
+﻿using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardAnim : MonoBehaviour
+{
+    [SerializeField] private float startYPos;
+    [SerializeField] private float duration;
+    [SerializeField] private Ease ease;
+    private Image cardSprite;
+
+    private void Start()
+    {
+        cardSprite = GetComponentInChildren<Image>();
+        cardAnimation();
+    }
+
+    private void cardAnimation()
+    {
+        cardSprite.rectTransform.anchoredPosition = new Vector3(0, startYPos, 0);
+        cardSprite.rectTransform.DOAnchorPosY(0f, duration).SetEase(ease);
+    }
+}
