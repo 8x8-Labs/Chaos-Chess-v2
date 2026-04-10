f7b888f56f7d6bb4d9e488fd998bb4a46e223092
Lee Siwoo <s24028@gsm.hs.kr>
2026-04-11 00:26:49 +0900

    feat: 카드 설명 활성화/비활성화 추가
    #15

---
+++ ChaosChess_v2/Assets/Script/UI/Ingame/CardDescPanel.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class CardDescPanel : ButtonPanel
+{
+    private GameManager gameManager = GameManager.Instance;
+
+    public override void DisablePanel()
+    {
+        base.DisablePanel();
+        if( gameManager == null ) gameManager = GameManager.Instance;
+        gameManager.IsGameInput = true;
+    }
+
+    public override void EnablePanel()
+    {
+        base.EnablePanel();
+        if( gameManager == null ) gameManager = GameManager.Instance;
+        gameManager.IsGameInput = false;
+    }
+}
