f7cb2f021efb266f747b95de3a04e62692ce48a2
nelu572 <s25031@gsm.hs.kr>
2026-04-12 03:05:28 +0900

    feat: 카드 보상에서 카드 선택 시 설명 패널 추가
    #8

---
+++ ChaosChess_v2/Assets/Script/UI/NotIngame/UICardAnim.cs
@@ -0,0 +1,51 @@
+﻿using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UICardAnim : MonoBehaviour
+{
+    [SerializeField] private float startYPos;
+    [SerializeField] private float duration;
+    [SerializeField] private Ease enableEase;
+    [SerializeField] private Ease disableEase;
+
+    private Ease disappearEase = Ease.InOutQuad;
+    private Image cardSprite;
+
+    private CardData cardData;
+    private UICardDescPanel panel;
+
+    private void Awake()
+    {
+        cardSprite = GetComponentInChildren<Image>();
+        cardData = GetComponent<CardData>();
+        panel = FindObjectOfType<UICardDescPanel>();
+        CardAnimation();
+    }
+
+    private void CardAnimation()
+    {
+        cardSprite.rectTransform.anchoredPosition = new Vector3(0, startYPos, 0);
+        cardSprite.rectTransform.DOAnchorPosY(0f, duration).SetEase(enableEase);
+    }
+
+    public void EnableCardDataUI()
+    {
+        panel.SetCardData(cardData);
+        panel.EnablePanel();
+    }
+
+    public void DestroyCard()
+    {
+        var rt = GetComponent<RectTransform>();
+        cardSprite.rectTransform.DOAnchorPosY(startYPos, duration / 2f)
+            .SetEase(disableEase)
+            .OnComplete(
+            () =>
+            {
+                rt.DOSizeDelta(new Vector2(0, rt.sizeDelta.y), 0.2f)
+                    .SetEase(disappearEase)
+                    .OnComplete(() => Destroy(gameObject));
+            });
+    }
+}
---
+++ ChaosChess_v2/Assets/Script/UI/NotIngame/UICardDescPanel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UICardDescPanel : ButtonPanel
+{
+    private GameManager gameManager = GameManager.Instance;
+
+    [Header("UI Elements")]
+    [SerializeField] private Image cardImage;
+    [SerializeField] private TMP_Text cardTitle;
+    [SerializeField] private TMP_Text cardDesc;
+
+    public override void DisablePanel()
+    {
+        base.DisablePanel();
+    }
+
+    public override void EnablePanel()
+    {
+        base.EnablePanel();
+    }
+
+    public void SetCardData(CardData data)
+    {
+        UpdateUI(data.DataSO);
+    }
+
+    private void UpdateUI(CardDataSO data)
+    {
+        cardImage.sprite = data.CardImage;
+        cardTitle.text = data.CardName;
+        cardDesc.text = data.CardDescription;
+    }
+}
