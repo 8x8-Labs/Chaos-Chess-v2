e0a36c377929fe9e4d0be54fdb67c8b8744f8746
nelu572 <s25031@gsm.hs.kr>
2026-04-20 03:26:01 +0900

    fix: 버프 디버프 SO 구분
    #8

---
+++ ChaosChess_v2/Assets/Script/GameCycle/Buff/Debuff/CardIntervalDebuff.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+/// <summary>
+/// 카드 지급 주기를 n만큼 늘리는 디버프
+/// </summary>
+[CreateAssetMenu(menuName = "Deuff/CardInterval")]
+public class CardIntervalDebuff : BuffSO
+{
+    public int reduction = 1;
+
+    public override void OnApply(Player player)
+    {
+        PlayerState.Instance.ModifyCardInterval(reduction);
+    }
+
+    public override void OnRemove(Player player)
+    {
+        PlayerState.Instance.ModifyCardInterval(reduction);
+    }
+}
