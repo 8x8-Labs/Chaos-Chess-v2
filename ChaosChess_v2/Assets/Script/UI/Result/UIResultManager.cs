3e591aeb95f8d966fb0b867a1b8bd4fb5d4de1a9
nelu572 <s25031@gsm.hs.kr>
2026-04-20 06:57:28 +0900

    feat: 결과 씬에 자신의 덱 보여주는 기능 구현
    #8

--- ChaosChess_v2/Assets/Script/UI/Result/UIResultManager.cs
+++ ChaosChess_v2/Assets/Script/UI/Result/UIResultManager.cs
@@ -1,11 +1,15 @@
+using System.Collections.Generic;
+using System.Linq;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIResultManager : MonoBehaviour
 {
     void Start()
     {
         UpdateRecordText();
+        SpawnDeckCardUI();
     }
 
 
@@ -20,5 +24,30 @@ public class UIResultManager : MonoBehaviour
         winValue.text = PlayerState.Instance.WinCount.ToString();
         drawValue.text = PlayerState.Instance.DrawCount.ToString();
         loseValue.text = PlayerState.Instance.LoseCount.ToString();
+    }
+
+    [SerializeField] private Transform content;
+    [SerializeField] private GameObject cardPrefab;
+
+    private void SpawnDeckCardUI()
+    {
+        if (PlayerState.Instance == null) return;
+
+        List<GameObject> cards = PlayerState.Instance.CardPool.ToList();
+
+        foreach (GameObject card in cards)
+        {
+            CardData cardData = card.GetComponent<CardData>();
+
+            GameObject cardObject = Instantiate(cardPrefab, content);
+
+            Image img = cardObject.GetComponentInChildren<Image>();
+            TextMeshProUGUI text = cardObject.GetComponentInChildren<TextMeshProUGUI>();
+
+            if (cardData.DataSO.CardImage != null)
+                img.sprite = cardData.DataSO.CardImage;
+
+            text.text = cardData.DataSO.CardName;
+        }
     }
 }
