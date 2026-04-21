24c1c2b43133a6aa635cfd9c4e81a3026fb66fe5
sxonz <203660235+sxonz@users.noreply.github.com>
2026-04-21 18:55:24 +0900

    fix: 풍차 버그 해결
    #101

--- ChaosChess_v2/Assets/Script/Card/skills/WindmillCard.cs
+++ ChaosChess_v2/Assets/Script/Card/skills/WindmillCard.cs
@@ -5,43 +5,34 @@ using UnityEngine;
 /// 풍차 - 기물 전용 (고급)
 /// 2턴 동안 룩은 비숍의 행마법대로, 비숍은 룩의 행마법대로 움직입니다.
 /// </summary>
-public class WindmillCard : CardData, IPieceCard
+public class WindmillCard : CardData, ICard
 {
-    private PieceSelector selector;
-
-    private void Awake()
-    {
-        selector = FindFirstObjectByType<PieceSelector>();
-    }
-
-    public void LoadPieceSelector()
-    {
-        if (selector == null) selector = FindFirstObjectByType<PieceSelector>();
-        selector.EnableSelector(this);
-    }
-
     public void Execute(CardEffectArgs args = null)
     {
-        Piece piece = args.Targets[0];
-        var effector = CreatePieceEffector<WindmillEffector>(piece);
-        effector.change = piece.Type == PieceType.Rook ? "b" : "r";
+        var effector = CreateGlobalEffector<WindmillEffector>();
+        foreach(Piece piece in BoardManager.Instance.GetAllPieces())
+        {
+            piece.MoveFenOverride = piece.Type == PieceType.Rook ? "b" : piece.Type == PieceType.Bishop ? "r" : null;
+            GameManager.Instance.AppendAction(DataSO.PieceLimitTurn, effector.Revert);
+        }
         effector.Apply();
-        GameManager.Instance.AppendAction(DataSO.PieceLimitTurn, effector.Revert);
 
     }
 }
-public class WindmillEffector : PieceEffector
+public class WindmillEffector : GlobalEffector
 {
-    public string change;
     protected override void OnApply()
     {
-        target.FenOverride = change;
         BoardManager.Instance.RefreshMoves();
     }
 
     protected override void OnRevert()
     {
-        target.FenOverride = null;
+        foreach (Piece piece in BoardManager.Instance.GetAllPieces())
+        {
+            if (piece.Type == PieceType.Bishop || piece.Type == PieceType.Rook)
+                piece.MoveFenOverride = null;
+        }
         BoardManager.Instance.RefreshMoves();
         Destroy(this);
     }
