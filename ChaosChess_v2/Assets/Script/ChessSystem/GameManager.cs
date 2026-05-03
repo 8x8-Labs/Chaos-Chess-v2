441b72cf2b49c917d7c9c814751143f95ab3a7f4
nelu572 <s25031@gsm.hs.kr>
2026-05-03 13:37:36 +0900

    feat: EvaluateGameState에 기물 부족 확인 로직 추가
    #143

--- ChaosChess_v2/Assets/Script/ChessSystem/GameManager.cs
+++ ChaosChess_v2/Assets/Script/ChessSystem/GameManager.cs
@@ -452,6 +452,10 @@ public class GameManager : MonoBehaviour
         {
             FinishType = GameResult.Draw;
         }
+        if (FairyStockfishBridge.Instance.IsInsufficientMaterial())
+        {
+            FinishType = GameResult.Draw;
+        }
         bool isCheck = FairyStockfishBridge.Instance.IsInCheck();
 
 
