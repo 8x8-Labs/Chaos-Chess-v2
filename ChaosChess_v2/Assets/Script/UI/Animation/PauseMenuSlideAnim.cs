0f5ec0887dd6d898d62ab9a955f574a87bc35614
Lee Siwoo <s24028@gsm.hs.kr>
2026-06-01 12:15:42 +0900

    feat(Anim): 일시정지 메뉴 UI 애니메이션 스크립트 3종 추가

---
+++ ChaosChess_v2/Assets/Script/UI/Animation/PauseMenuSlideAnim.cs
@@ -0,0 +1,43 @@
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PauseMenuSlideAnim : MonoBehaviour, IUIAnimation
+{
+    public float Duration
+    {
+        get => duration;
+        set => duration = value;
+    }
+
+    [SerializeField] private RectTransform target;
+    [SerializeField] private float duration = 0.4f;
+    [SerializeField] private float startOffsetX = 600f;
+    [SerializeField] private Ease startAnimEase = Ease.OutCubic;
+    [SerializeField] private Ease endAnimEase = Ease.InCubic;
+    [SerializeField] private UnityEvent OnAnimationStart;
+    [SerializeField] private UnityEvent OnAnimationEnd;
+
+    public void StartAnimation(float delay)
+    {
+        target.DOKill(true);
+        target.anchoredPosition = new Vector2(startOffsetX, target.anchoredPosition.y);
+        target.DOAnchorPosX(0f, duration)
+            .SetEase(startAnimEase)
+            .SetDelay(delay)
+            .OnStart(() => OnAnimationStart?.Invoke());
+    }
+
+    public void EndAnimation()
+    {
+        target.DOKill(true);
+        target.DOAnchorPosX(startOffsetX, duration)
+            .SetEase(endAnimEase)
+            .OnComplete(() => OnAnimationEnd?.Invoke());
+    }
+
+    private void OnDestroy()
+    {
+        target?.DOKill();
+    }
+}
