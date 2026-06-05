f87005fb244a8821e7c58df5be52fac36a33524f
nelu572 <s25031@gsm.hs.kr>
2026-06-05 19:25:57 +0900

    fix: 카드 개수 7개로 변경
    #198

--- ChaosChess_v2/Assets/Script/UI/NotIngame/StartDeckManager.cs
+++ ChaosChess_v2/Assets/Script/UI/NotIngame/StartDeckManager.cs
@@ -6,7 +6,7 @@ public class StartDeckManager : MonoBehaviour
 {
     [SerializeField] private UICardPanel uiCardPanel;
     [SerializeField] private Button rerollButton;
-    [SerializeField] private int commonCardCount = 2;
+    [SerializeField] private int commonCardCount = 4;
     [SerializeField] private int uncommonCardCount = 2;
     [SerializeField] private int uniqueCardCount = 1;
     [SerializeField] private float rerollCardInterval = 0.12f;
