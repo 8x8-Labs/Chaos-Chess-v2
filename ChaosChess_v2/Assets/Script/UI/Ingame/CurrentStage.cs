43dcce615445a3c4366b531eb9a0ef7051c4ee7e
Lee Siwoo <s24028@gsm.hs.kr>
2026-06-07 03:11:16 +0900

    feat(UI): CurrentStage 및 MapParallax UI 스크립트 추가

---
+++ ChaosChess_v2/Assets/Script/UI/Ingame/CurrentStage.cs
@@ -0,0 +1,11 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class CurrentStage : MonoBehaviour
+{
+    [SerializeField] private TMP_Text titleText;
+    private void Start()
+    {
+        titleText.text = MapManager.Instance?.curMap?.MapName;
+    }
+}
